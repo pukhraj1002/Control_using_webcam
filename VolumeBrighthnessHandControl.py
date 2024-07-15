30e3f87a8e791006dc83b2d0900e8c278e4bedfc
Pukhraj Kushwah <114336304+pukhraj1002@users.noreply.github.com>
2024-07-15 10:29:02 +0530

    Update VolumeBrighthnessHandControl.py

--- VolumeBrighthnessHandControl.py
+++ VolumeBrighthnessHandControl.py
@@ -1,13 +1,39 @@
 import cv2
 import time
 import numpy as np
-import HandTrackingModule as htm
+import mediapipe as mp
 import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import screen_brightness_control as sbc
 
+class HandDetector:
+    def __init__(self):
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        return lmList
 
 #########################
 wCam, hCam = 640, 480
@@ -18,18 +44,15 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector()
-
+detector = HandDetector()
 
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 current_brightness = sbc.get_brightness()
 
 volRange = volume.GetVolumeRange()
-
 
 minVol = volRange[0]
 maxVol = volRange[1]
@@ -44,23 +67,23 @@ bright = 0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw = False)
+    lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
         #print(lmList[4], lmList[8])
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        cv2.circle(img, (x1, y1), 15, (255,0,255),cv2.FILLED)
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
         cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-        length = math.hypot(x2-x1, y2-y1)
+        length = math.hypot(x2 - x1, y2 - y1)
         #print(length)
 
-        #230, 18
-        #volRange -65, 0
+        # 230, 18
+        # volRange -65, 0
 
         vol = np.interp(length, [18, 230], [minVol, maxVol])
         volBar = np.interp(length, [18, 230], [400, 150])
@@ -68,26 +91,23 @@ while True:
         print(vol)
         volume.SetMasterVolumeLevel(vol, None)
         bright = np.interp(length, [18, 230], [minBright, maxBright])
-        sbc.set_brightness(bright, None)
+        sbc.set_brightness(bright)
 
-
-        if length<30:
+        if length < 30:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
 
-
-    cv2.rectangle(img, (50,150), (85, 400), (255, 0, 0), 3)
-    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0),cv2.FILLED)
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
     cv2.putText(img, f'{int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
-
     cTime = time.time()
-    fps = 1/ (cTime - pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
